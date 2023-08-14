f22ea668bc7b7c234e6f885417f885c1d7299a39
StepanTheGreat <grettanumber900@gmail.com>
2023-08-14 22:09:28 +0200

    Password generator

---
+++ libs/password-generator/password_generator.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} pass
+* @param {string} chars
+* @param {number} length
+* @returns {string}
+*/
+export function generate_password(pass: string, chars: string, length: number): string;
--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,15 @@
+<script lang="ts">
+  import { generate_password } from "password-generator";
+
+  let phrase = "";
+  let pass = generate_password(phrase, "qwrtyuiop[", 40);
+  function onInput() {
+    pass = generate_password(phrase, "qwrtyuiop[", 40);
+  }
+
+</script>
+
 <main>
-  <h1 class="text-blue-500">Welcome to the app!</h1>
+  <h1 class="text-blue-500">{pass}</h1>
+  <input bind:value={phrase} on:input={onInput}/>
 </main>
