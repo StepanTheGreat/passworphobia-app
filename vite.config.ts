790daeabc5b2d1d95b36fe96232d7c028df00acd
StepanTheGreat <grettanumber900@gmail.com>
2023-08-14 22:11:00 +0200

    Top level await plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig(async () => ({
   plugins: [
     svelte(),
-    wasm()
+    wasm(),
+    topLevelAwait()
   ],
   clearScreen: false,
   server: {
