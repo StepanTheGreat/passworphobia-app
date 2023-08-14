28b1be456e0898b01cc7fb34f0d75956dcdd5a0d
StepanTheGreat <grettanumber900@gmail.com>
2023-08-14 22:21:59 +0200

    Basic password

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
   import { generate_password } from "password-generator";
 
-  let phrase = "";
-  let pass = generate_password(phrase, "qwrtyuiop[", 40);
-  function onInput() {
-    pass = generate_password(phrase, "qwrtyuiop[", 40);
-  }
+  const chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789";
 
+  let phrase = "";
+  let pass = generate_password(phrase, chars, 40);
+  function onInput() {
+    if (phrase) {
+      pass = generate_password(phrase, chars, 40);
+    } else {
+      pass = "";
+    }
+    
+  }
 </script>
 
-<main>
-  <h1 class="text-blue-500">{pass}</h1>
-  <input bind:value={phrase} on:input={onInput}/>
+<main class="w-full flex">
+  <div class="w-fit mx-auto text-center">
+    <h1 class="text-blue-500">{pass}</h1>
+    <input 
+      type="text" 
+      bind:value={phrase} on:input={onInput}
+      class="border border-black w-fit"
+    />
+  </div>
+  
 </main>
