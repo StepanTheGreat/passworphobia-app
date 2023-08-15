c7d6fa77eb19c9fff75e4ff898386723e0630697
StepanTheGreat <grettanumber900@gmail.com>
2023-08-15 22:03:15 +0200

    Better UI

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,20 @@
     import Generator from "./components/Generator.svelte";
 </script>
 
-<main class="w-full h-full flex flex-col bg-secondary">
-  <h1 class="text-white text-5xl mx-auto font-bold mb-16">Passworphobia</h1>
-  <Generator></Generator>
+<main class="w-full h-full grid bg-secondary">
+  <div class="flex">
+    <h1 class="text-white text-5xl font-bold m-auto">Passworphobia</h1>
+  </div>
+  <div class="flex flex-col">
+    <Generator></Generator>
+  </div>
+  <div></div>
 </main>
+
+<style>
+  main {
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 2fr 1fr;
+    gap: 16px;
+  }
+</style>
