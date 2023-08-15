d20efa7411a729bcb8e3f7e3e465969606e6da3b
minianita <anieruste06@gmail.com>
2023-08-15 16:23:47 +0300

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: { 'text': '#faf4ef',
+    'background': '#13142c',
+    'primary': '#1c21aa',
+    'secondary': '#140059',
+    'accent': '#ff7a1c',},
   },
   plugins: [],
 }
