8f034c3b58c423b93fcd14a19c2828f99d0197f9
StepanTheGreat <grettanumber900@gmail.com>
2023-08-15 15:24:26 +0200

    Epic correction

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,13 @@
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: { 'text': '#faf4ef',
-    'background': '#13142c',
-    'primary': '#1c21aa',
-    'secondary': '#140059',
-    'accent': '#ff7a1c',},
+    extend: { 
+      "text": "#faf4ef",
+      "background": "#13142c",
+      "primary": "#1c21aa",
+      "secondary": "#140059",
+      "accent": "#ff7a1c",
+    },
   },
   plugins: [],
 }
