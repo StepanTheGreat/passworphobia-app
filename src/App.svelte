71f755de1eac3499572b2aaa184b77b48312fe3b
StepanTheGreat <117771383+StepanTheGreat@users.noreply.github.com>
2023-08-15 15:22:39 +0200

    Generator

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,8 @@
 <script lang="ts">
-  import { generate_password } from "password-generator";
+    import Generator from "./components/Generator.svelte";
 
-  const chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789";
-
-  let phrase = "";
-  let pass = "";
-  function onInput() {
-    pass = (phrase) ? generate_password(phrase, chars, 40) : "";
-  }
 </script>
 
 <main class="w-full flex">
-  <div class="w-fit mx-auto text-center">
-    <h1 class="text-blue-500">{pass}</h1>
-    <input 
-      type="text" 
-      bind:value={phrase} on:input={onInput}
-      class="border border-black w-fit"
-    />
-  </div>
-  
+  <Generator></Generator>
 </main>
