2962a9de9357a4b97c637888c490336e8ec565a8
minianita <anieruste06@gmail.com>
2023-08-22 22:35:48 +0300

    color

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -1,4 +1,4 @@
 <div class="whitespace-normal">
-    <h1 class="text-transparent inline bg-clip-text bg-gradient-to-r from-text via-blue-500 to-accent m-0">Passwor</h1>
-    <h1 class="text-transparent inline bg-clip-text bg-gradient-to-r from-text via-blue-500 to-accent m-0">phobia</h1>
+    <h1 class="text-8xl font-bold text-transparent inline bg-clip-text bg-gradient-to-r from-text from-40% via-primary via-55% to-accent to-99% m-0">Passworphobia</h1>
+    <!-- <h1 class="text-transparent inline bg-clip-text bg-gradient-to-r from-text via-blue-500 to-accent m-0">phobia</h1> -->
 </div>
