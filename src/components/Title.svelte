bc8dc3745249f29a09b383b51a2ac3390c4a2a2d
StepanTheGreat <grettanumber900@gmail.com>
2023-08-22 21:07:23 +0200

    Some changes

---
+++ src/components/Title.svelte
@@ -0,0 +1,4 @@
+<div class="whitespace-normal">
+    <h1 class="text-transparent inline bg-clip-text bg-gradient-to-r from-text via-blue-500 to-accent m-0">Passwor</h1>
+    <h1 class="text-transparent inline bg-clip-text bg-gradient-to-r from-text via-blue-500 to-accent m-0">phobia</h1>
+</div>
