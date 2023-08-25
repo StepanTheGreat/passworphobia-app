f9d23c3f3c1d80b5a0e4d0bda4f78707d3f80805
StepanTheGreat <grettanumber900@gmail.com>
2023-08-25 18:36:44 +0200

    Copyright and lang change

---
+++ src/components/LangBtn.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { getLang, langLoad, saveLang } from "../lang";
+    export let lang: string = getLang();
+
+    let langs = {
+        "en": "EN", 
+        "ua": "UA", 
+        "et": "ET", 
+        "fr": "FR", 
+        "ru": "RU"
+    };
+
+    function changeLang() {
+        let langArr = Object.keys(langs);
+        let ind = langArr.indexOf(lang);
+        ind = (ind+1) % langArr.length;
+        lang = langArr[ind];
+
+        langLoad(lang);
+        saveLang(lang);
+    }
+</script>
+
+<button class="my-auto mx-8 w-fit h-fit text-4xl text-text" on:click={changeLang}>{langs[lang]}</button>
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+// Copyright © 2023 August, Anita Eruste and Stepan Malchenko. All rights reserved.
+
 import "./app.css";
 import App from "./App.svelte";
 
