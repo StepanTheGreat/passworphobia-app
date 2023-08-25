86f126c1ded5f69624c598dad73daa78cd454ad6
StepanTheGreat <grettanumber900@gmail.com>
2023-08-25 18:36:49 +0200

    Copyright for our rust code source

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+// Copyright © 2023 August, Anita Eruste and Stepan Malchenko. All rights reserved.
+
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
