aa0563a1e9850322ed8dcf9035257283b3d25b82
StepanTheGreat <117771383+StepanTheGreat@users.noreply.github.com>
2023-09-24 14:31:21 +0200

    An empty string

--- src/components/Main/GeneratorSettings.svelte
+++ src/components/Main/GeneratorSettings.svelte
@@ -6,6 +6,7 @@
     let hidden: boolean = true;
 
     let text: {[key: string]: string|string[]} = {
+        "":"",
         sizeHint: [],
         settingsNumbers: "",
         settingsScharacters: ""
