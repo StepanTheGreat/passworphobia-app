85a7b07e9f440a5c7687b255c9416651d2d4ace2
StepanTheGreat <117771383+StepanTheGreat@users.noreply.github.com>
2023-10-26 14:24:30 +0200

    Language update and more concise hints

--- src/components/Main/PassOutput.svelte
+++ src/components/Main/PassOutput.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
+    import { langTable } from "../../lang";
     import { storePassOutput } from "../../store";
     import VisibilityButton from "../VisibilityButton.svelte";
     import { onDestroy } from "svelte";
 
     export let size: number = 14;
+
+    let outputPlaceholderText: string;
+
+    let unsubscribeLang = langTable.subscribe(newTable => {
+        outputPlaceholderText = newTable.generator.outputPlaceholder;
+    });
+
     let visible: boolean = false;
     let outputValue: string = "";
 
     let unsubscribe = storePassOutput.subscribe(newValue => outputValue = newValue);
-    onDestroy(() => unsubscribe());
+    onDestroy(() => {
+        unsubscribeLang();
+        unsubscribe();
+    });
 </script>
 
 <div class="flex flex-row m-1 mb-6">
@@ -17,6 +28,7 @@
         type={visible ? "text" : "password"} 
         value={outputValue}
         disabled
+        placeholder={outputPlaceholderText}
         class="text-center text-text rounded-md mx-1 bg-primary-dark pr-8 pl-1"
     />
     <VisibilityButton {visible} callback={() => visible = !visible}></VisibilityButton>
--- src/components/Settings/Settings.svelte
+++ src/components/Settings/Settings.svelte
@@ -20,11 +20,11 @@
         apply: "",
         save: "",
         password: "",
-        newPassword: "",
-        confirmPassword: "",
+        newPasswordHint: "",
         successNewPassword: "",
         nothingToSave: "",
         settingsSaved: "",
+        confirmPasswordHint: "",
         confirmPasswordDoesntMatch: "",
         notMatchingPasswords: "",
         emptyPassword: "",
@@ -133,7 +133,7 @@
         <h1 class="text-text text-4xl text-center">{text.password}</h1>
         <div class="my-4">
             <div class="mt-4 flex flex-col">
-                <p class="text-text text-md text-center my-1">{text.newPassword}</p>
+                <p class="text-text italic text-md text-center my-1 mb-3">{text.newPasswordHint}</p>
                 <LockPass size={32} on:passInput={e => onPassInput(e, "new1")}></LockPass>
                 <LockPass size={32} on:passInput={e => onPassInput(e, "new2")}></LockPass>
             </div>
@@ -145,7 +145,7 @@
     </div>
 
     <div class="w-full flex flex-col mb-8">
-        <p class="text-text text-md text-center my-1">{text.confirmPassword}</p>
+        <p class="text-text italic text-center mb-2">{text.confirmPasswordHint}</p>
         <LockPass size={32} on:passInput={e => onPassInput(e, "old")}></LockPass>
         <p class="{confirmationHint.color} italic text-center mb-2">{text[confirmationHint.key]}</p>
         <button class="bg-accent text-text px-16 py-4 rounded-xl mx-auto" on:click={saveSettings}>Save</button>
